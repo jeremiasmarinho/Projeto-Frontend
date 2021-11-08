283084b07805be5f98ac4662b132c540d30d93ba
Jeremias Marinho <jeremias10@msn.com>
2021-11-08 00:15:14 -0300

    finalizando as pagina de tarefas

--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -1,7 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import api from '../../services/api';
+
+import moment from 'moment';
+
+interface ITask {
+  id: number;
+  title: string;
+  description: string;
+  finished: boolean;
+  created_at: Date;
+  update_at: Date;
+}
+
+
+
 
 const Tasks: React.FC = () => {
-  return <h1>Tasks page</h1>
+
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  useEffect(() => {
+    loadTasks()
+  }, [])
+  
+  async function loadTasks(){
+        
+    const response = await api.get('/tasks')
+    console.log(response)
+    setTasks(response.data)
+  
+  }
+ 
+  function formateDate(date: Date){
+    return moment(date).format("DD/MM/YYYY")
+  }
+
+  return (
+    
+    <div className="container">
+      
+        <br/>
+        <h1>Lista de Tarefas</h1>
+   
+        <Table striped bordered hover className="text-center">
+            
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Data de Atualização</th>
+                <th>Status</th>
+                <th>Ações</th>
+                
+              </tr>
+              </thead>
+              <tbody>
+                {
+                  
+
+                  tasks.map(task => (
+                    <tr key={task.id}>
+                    <td>{ task.id }</td>
+                    <td>{ task.title }</td>
+                    <td>{ formateDate(task.update_at)}</td>
+                   <td>
+
+                   <Button size="sm" disabled variant={ task.finished ? "success" : "warning"}>
+                     {task.finished ? "FINALIZADO" : "PENDENTE"}
+
+                   </Button>
+
+                    </td>
+                    <td> 
+                      <Button size="sm">Editar</Button>{' '} 
+                      <Button size="sm" variant="success">Finalizar</Button> {' '} 
+                      <Button size="sm" variant="info">Visualizar</Button> {' '} 
+                      <Button size="sm" variant="danger">Remover</Button> 
+                      </td>            
+                    </tr>
+                  ))
+                }
+               
+              </tbody>
+            
+
+        </Table>
+   
+    </div>   
+
+    
+
+        )
 }
 
 export default Tasks;
